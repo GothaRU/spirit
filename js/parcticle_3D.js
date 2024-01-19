3fb3dbac5e807d4cf69aedecf90e9f2005a7e43f
GothaRU <gotha111128@gmail.com>
2024-01-19 21:27:20 +0300

    подготовка

---
+++ js/parcticle_3D.js
@@ -0,0 +1,9 @@
+//<script type="importmap">
+//{
+//    "imports": {
+//      "three": "https://unpkg.com/three@<version>/build/three.module.js",
+//      "three/addons/": "https://unpkg.com/three@<version>/examples/jsm/"
+//    }
+//  }
+//</script>
+
